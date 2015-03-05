7e766db82858b853cbafe04046f1f86e63221a52
dojah <dojahsdojo@gmail.com>
2015-03-04 21:17:14 -0500

    SweetBro

---
+++ main.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+console.log("hello world!");
+
+var a = 6;
+
+var b = square(a);
+
+console.log(b);
+
+var text = fs.readFileSync("hi.txt", "utf-8");
+console.log(text);
+
+function square(a){
+	return a*a;
+}
+
+console.log("Sweet bro.");
